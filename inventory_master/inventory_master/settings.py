aed169b68d3c52f2ebf2811959205dc09488c519
Akkanat Ashyrepov <119655536+AkkAshy@users.noreply.github.com>
2025-06-28 14:10:17 +0500

    Update settings.py

--- inventory_master/inventory_master/settings.py
+++ inventory_master/inventory_master/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-3)79-7^5@ww@79xk2s&koc4_tb4ay)r$4#+)qja@mp37(0hm-w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['invenmaster.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
